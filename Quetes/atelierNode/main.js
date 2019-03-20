1efd6fbd5d444cfd1b3d5d8d2d78b311e758fef6
Valentin Pawlowski <valentinp821@gmail.com>
2019-03-20 10:29:00 +0100

    ok

---
+++ Quetes/atelierNode/main.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Remy Teissier'));
+console.log(chalk.green('Omar El Bouslamti'));
+console.log(chalk.red('Elliot B'));
+console.log(chalk.magenta('Emilie B'));
